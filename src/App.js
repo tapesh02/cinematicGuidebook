54644aacba081994d2c704e74b01acc1cb84d762
tapesh02 <34514858+tapesh02@users.noreply.github.com>
2023-10-12 23:20:16 +0100

    Added initial setup

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.scss";
+
+const App = () => {
+    return (
+        <>
+            <h2>cinematicGuidebook</h2>
+            <p>
+                Your one-stop shop for movie and TV show trailers, ratings, and more. This description is clear,
+                concise, and informative, and it accurately reflects the purpose of your repo. It also uses relevant
+                keywords that potential users are likely to search for, such as "movie trailers", "TV show trailers",
+                and "ratings".
+            </p>
+        </>
+    );
+};
+export default App;
