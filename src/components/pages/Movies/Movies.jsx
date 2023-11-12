13e211a7f2f49db43f50907879ebf64be4aa0336
tapesh02 <34514858+tapesh02@users.noreply.github.com>
2023-11-12 23:51:07 +0000

    Added process env and background on movies page

--- src/components/pages/Movies/Movies.jsx
+++ src/components/pages/Movies/Movies.jsx
@@ -3,30 +3,34 @@ import axios from "axios";
 import Cards from "../../Cards/Cards";
 
 const Movies = () => {
+    const [isLoading, setIsLoading] = useState(true);
     const [movies, setMovies] = useState([]);
 
     const fetchMovies = async () => {
-        const requestConfig = {
-            method: "GET",
-            url: "https://api.themoviedb.org/3/discover/movie",
-            params: {
-                include_adult: "false",
-                include_video: "false",
-                language: "en-US",
-                page: "1",
-                sort_by: "popularity.desc",
-            },
-            headers: {
-                accept: "application/json",
-                Authorization:
-                    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0YTU4ZWQ1NzkzZGZkZDVmMmYxZWZjMjA2OWNkYzExMiIsInN1YiI6IjYwNWI0ODUxN2RmZGE2MDAyOGZlZjA3ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-sSCnDCqigw-Tk_cMg07MbphQBUGLd3E93z7SLptm04",
-            },
-        };
-        const response = await axios.request(requestConfig);
+        try {
+            const requestConfig = {
+                method: "GET",
+                url: "https://api.themoviedb.org/3/discover/movie",
+                params: {
+                    include_adult: "false",
+                    include_video: "false",
+                    language: "en-US",
+                    page: "1",
+                    sort_by: "popularity.desc",
+                },
+                headers: {
+                    accept: "application/json",
+                    Authorization: `Bearer${process.env.REACT_APP_API_KEY}`,
+                },
+            };
+            const response = await axios.request(requestConfig);
 
-        if (response) {
-            setMovies(response.data.results);
-            console.log(response.data.results);
+            if (response) {
+                setMovies(response.data.results);
+                setIsLoading(false);
+            }
+        } catch (error) {
+            console.log("can not fetch movies");
         }
     };
 
@@ -35,23 +39,30 @@ const Movies = () => {
     }, []);
 
     return (
-        <div className="mainMovies">
-            <div className="homeCardsBg moviesBg">
-                <div className="cardMain">
-                    {movies?.map((movie) => {
-                        const { poster_path, id } = movie;
-                        return (
-                            <Cards
-                                key={id}
-                                classname="movieCards"
-                                cardImage={`https://image.tmdb.org/t/p/original${poster_path}`}
-                                id={id}
-                            />
-                        );
-                    })}
-                </div>
-            </div>
-        </div>
+        <>
+            {isLoading ? (
+                "Isloading"
+            ) : (
+                <>
+                    <div className="mainMovies"></div>
+                    <div className="homeCardsBg moviesBg">
+                        <div className="cardMain">
+                            {movies?.map((movie) => {
+                                const { poster_path, id } = movie;
+                                return (
+                                    <Cards
+                                        key={id}
+                                        classname="movieCards"
+                                        cardImage={`https://image.tmdb.org/t/p/original${poster_path}`}
+                                        id={id}
+                                    />
+                                );
+                            })}
+                        </div>
+                    </div>
+                </>
+            )}
+        </>
     );
 };
 
