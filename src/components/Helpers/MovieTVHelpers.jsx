bace7a1d49c6fdcc51a5afd77f358360788d0ff2
tapesh02 <34514858+tapesh02@users.noreply.github.com>
2024-03-12 02:45:45 +0000

    Added helper function

--- src/components/Helpers/MovieTVHelpers.jsx
+++ src/components/Helpers/MovieTVHelpers.jsx
@@ -1,4 +1,4 @@
-import { Typography } from "@mui/material";
+import { Skeleton, Typography } from "@mui/material";
 import Carousel from "../Carousal/Carousal";
 
 export const renderTopRated = (searchInput, topRated) =>
@@ -8,3 +8,20 @@ export const renderTopRated = (searchInput, topRated) =>
             <Carousel slides={topRated} />
         </>
     );
+
+export const renderSkeleton = (num) => {
+    const skeletons = [];
+    for (let i = 0; i < num; i++) {
+        skeletons[i] = (
+            <Skeleton
+                key={i}
+                animation="pulse"
+                height={200}
+                variant="rectangular"
+                width={130}
+                sx={{ animationDuration: "1.5s", borderRadius: "5px" }}
+            />
+        );
+    }
+    return skeletons;
+};
