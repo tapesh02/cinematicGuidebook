c3a81f96ea23bad34e780bad335b87fa0b4a6647
tapesh02 <34514858+tapesh02@users.noreply.github.com>
2024-03-20 20:47:26 +0000

    Fixed cors errors

--- server/index.js
+++ server/index.js
@@ -10,8 +10,8 @@ const app = express();
 app.use(express.json());
 
 const corsOptions = {
-  origin: ["http://localhost:3000"],
-  methods: ["POST. GET, DELETE, PATCH"],
+  origin: [`${process.env.BACKEND_URL}`],
+  methods: ["POST", "PATCH", "DELETE", "OPTIONS", "GET"],
   credentials: true,
   headers: "Content-Type",
 };
