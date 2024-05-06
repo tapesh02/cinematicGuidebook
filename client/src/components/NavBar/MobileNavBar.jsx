28bb4844e3217656a1bae08695ace883cae6a56a
tapesh02 <34514858+tapesh02@users.noreply.github.com>
2024-05-06 12:08:05 +0100

    Added Details Section

--- client/src/components/NavBar/MobileNavBar.jsx
+++ client/src/components/NavBar/MobileNavBar.jsx
@@ -3,14 +3,7 @@ import * as React from "react";
 import { NavLink } from "react-router-dom";
 
 import Box from "@mui/material/Box";
-import Drawer from "@mui/material/Drawer";
-import Button from "@mui/material/Button";
-import List from "@mui/material/List";
-import Divider from "@mui/material/Divider";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
+import { Drawer, List, Divider, ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
 
 import logo from "../../assets/logos/TextLogo.svg";
 
@@ -18,89 +11,89 @@ import { AiFillHome, AiFillContacts, AiOutlineTeam, AiOutlineSearch } from "reac
 import { BsFillAwardFill, BsMenuButtonWideFill } from "react-icons/bs";
 
 export default function MobileNavBar() {
-    const [state, setState] = React.useState({
-        left: false,
-    });
+  const [state, setState] = React.useState({
+    left: false,
+  });
 
-    const toggleDrawer = (anchor, open) => (event) => {
-        if (event.type === "keydown" && (event.key === "Tab" || event.key === "Shift")) {
-            return;
-        }
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (event.type === "keydown" && (event.key === "Tab" || event.key === "Shift")) {
+      return;
+    }
 
-        setState({ ...state, [anchor]: open });
-    };
+    setState({ ...state, [anchor]: open });
+  };
 
-    const list = (anchor) => (
-        <Box
-            sx={{
-                width: anchor === "left" ? "auto" : 250,
-                height: "100%",
-                backgroundColor: "#010101",
-                outlinne: "none",
-            }}
-            role="presentation"
-            onClick={toggleDrawer(anchor, false)}
-            onKeyDown={toggleDrawer(anchor, false)}>
-            <List>
-                <ListItem disablePadding>
-                    <ListItemButton>
-                        <ListItemIcon>
-                            <AiFillHome className="iconStyle" />
-                        </ListItemIcon>
-                        <ListItemText primary="Movies" />
-                    </ListItemButton>
-                </ListItem>
+  const list = (anchor) => (
+    <Box
+      sx={{
+        width: anchor === "left" ? "auto" : 250,
+        height: "100%",
+        backgroundColor: "#010101",
+        outlinne: "none",
+      }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}>
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <AiFillHome className="iconStyle" />
+            </ListItemIcon>
+            <ListItemText primary="Movies" />
+          </ListItemButton>
+        </ListItem>
 
-                <ListItem disablePadding>
-                    <ListItemButton>
-                        <ListItemIcon>
-                            <AiOutlineTeam className="iconStyle" />
-                        </ListItemIcon>
-                        <ListItemText primary="TVshows" />
-                    </ListItemButton>
-                </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <AiOutlineTeam className="iconStyle" />
+            </ListItemIcon>
+            <ListItemText primary="TVshows" />
+          </ListItemButton>
+        </ListItem>
 
-                <ListItem disablePadding>
-                    <ListItemButton>
-                        <ListItemIcon>
-                            <AiFillContacts className="iconStyle" />
-                        </ListItemIcon>
-                        <ListItemText primary="Trending" />
-                    </ListItemButton>
-                </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <AiFillContacts className="iconStyle" />
+            </ListItemIcon>
+            <ListItemText primary="Trending" />
+          </ListItemButton>
+        </ListItem>
 
-                <ListItem disablePadding>
-                    <ListItemButton>
-                        <ListItemIcon>
-                            <BsFillAwardFill className="iconStyle" />
-                        </ListItemIcon>
-                        <ListItemText primary="Profile" />
-                    </ListItemButton>
-                </ListItem>
-            </List>
-            <Divider />
-            <div style={{ width: "100%" }}>
-                <input placeholder="search" />
-            </div>
-        </Box>
-    );
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <BsFillAwardFill className="iconStyle" />
+            </ListItemIcon>
+            <ListItemText primary="Profile" />
+          </ListItemButton>
+        </ListItem>
+      </List>
+      <Divider />
+      <div style={{ width: "100%" }}>
+        <input placeholder="search" />
+      </div>
+    </Box>
+  );
 
-    return (
-        <div className="mobileMenu">
-            {["left"].map((anchor) => (
-                <React.Fragment key={anchor}>
-                    <BsMenuButtonWideFill onClick={toggleDrawer(anchor, true)} className="menuIcon">
-                        {anchor}
-                    </BsMenuButtonWideFill>
-                    <Drawer anchor={anchor} open={state[anchor]} onClose={toggleDrawer(anchor, false)}>
-                        {list(anchor)}
-                    </Drawer>
-                    <NavLink>
-                        <img src={logo} alt="logo" />
-                    </NavLink>
-                    <AiOutlineSearch className="menuIcon" />
-                </React.Fragment>
-            ))}
-        </div>
-    );
+  return (
+    <div className="mobileMenu">
+      {["left"].map((anchor) => (
+        <React.Fragment key={anchor}>
+          <BsMenuButtonWideFill onClick={toggleDrawer(anchor, true)} className="menuIcon">
+            {anchor}
+          </BsMenuButtonWideFill>
+          <Drawer anchor={anchor} open={state[anchor]} onClose={toggleDrawer(anchor, false)}>
+            {list(anchor)}
+          </Drawer>
+          <NavLink>
+            <img src={logo} alt="logo" />
+          </NavLink>
+          <AiOutlineSearch className="menuIcon" />
+        </React.Fragment>
+      ))}
+    </div>
+  );
 }
