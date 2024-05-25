2ea2b71ee4621d2ef3e4151a49937d1bb56451a8
tapesh02 <34514858+tapesh02@users.noreply.github.com>
2024-05-25 12:04:59 +0100

    fix: style naming convention

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -12,68 +12,60 @@ import { FaUserCircle } from "react-icons/fa";
 import { AppBar, Box, Button, Container, Toolbar } from "@mui/material";
 
 const NavBar = () => {
-    const theme = useTheme();
-    const isMobile = useMediaQuery(theme.breakpoints.down("md"));
-    const { isLoggedIn } = useContext(GlobalContext);
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+  const { isLoggedIn } = useContext(GlobalContext);
 
-    const [showMenuItem, setShowMenuItem] = useState(false);
+  const [showMenuItem, setShowMenuItem] = useState(false);
 
-    const pageLinks = ["Home", "Movies", "TvShows"];
+  const pageLinks = ["Home", "Movies", "TvShows"];
 
-    return (
-        <>
-            {!isMobile ? (
-                <AppBar color="transparent" position="static" className="appBar">
-                    <Container maxWidth="xl">
-                        <Toolbar className="toolBar">
-                            <Box>
-                                <NavLink to={"/"}>
-                                    <img src={logo} alt="logo" />
-                                </NavLink>
-                            </Box>
-                            {isLoggedIn && (
-                                <Box className="navBar">
-                                    {pageLinks.map((page) => (
-                                        <NavLink
-                                            to={page.toLocaleLowerCase() === "home" ? "/" : page.toLocaleLowerCase()}
-                                            key={page}>
-                                            <Button variant="outlined" color="secondary">
-                                                {page}
-                                            </Button>
-                                        </NavLink>
-                                    ))}
-                                    <FaUserCircle
-                                        size={30}
-                                        onClick={() => setShowMenuItem(!showMenuItem)}
-                                        className="menu-icon"
-                                    />
-                                    {showMenuItem && (
-                                        <SubMenu showMenuItem={showMenuItem} setShowMenuItem={setShowMenuItem} />
-                                    )}
-                                </Box>
-                            )}
-                            {!isLoggedIn && (
-                                <Box className="navBar">
-                                    <NavLink to={"/signin"}>
-                                        <Button variant="outlined" color="secondary">
-                                            Sign In
-                                        </Button>
-                                    </NavLink>
-                                    <NavLink to={"/joinus"}>
-                                        <Button variant="contained" color="secondary">
-                                            Join Us
-                                        </Button>
-                                    </NavLink>
-                                </Box>
-                            )}
-                        </Toolbar>
-                    </Container>
-                </AppBar>
-            ) : (
-                <MobileNavBar />
-            )}
-        </>
-    );
+  return (
+    <>
+      {!isMobile ? (
+        <AppBar color="transparent" position="static" className="app-bar">
+          <Container maxWidth="xl">
+            <Toolbar className="tool-bar">
+              <Box>
+                <NavLink to={"/"}>
+                  <img src={logo} alt="logo" />
+                </NavLink>
+              </Box>
+              {isLoggedIn && (
+                <Box className="nav-bar">
+                  {pageLinks.map((page) => (
+                    <NavLink to={page.toLocaleLowerCase() === "home" ? "/" : page.toLocaleLowerCase()} key={page}>
+                      <Button variant="outlined" color="secondary">
+                        {page}
+                      </Button>
+                    </NavLink>
+                  ))}
+                  <FaUserCircle size={30} onClick={() => setShowMenuItem(!showMenuItem)} className="menu-icon" />
+                  {showMenuItem && <SubMenu showMenuItem={showMenuItem} setShowMenuItem={setShowMenuItem} />}
+                </Box>
+              )}
+              {!isLoggedIn && (
+                <Box className="nav-bar">
+                  <NavLink to={"/signin"}>
+                    <Button variant="outlined" color="secondary">
+                      Sign In
+                    </Button>
+                  </NavLink>
+                  <NavLink to={"/joinus"}>
+                    <Button variant="contained" color="secondary">
+                      Join Us
+                    </Button>
+                  </NavLink>
+                </Box>
+              )}
+            </Toolbar>
+          </Container>
+        </AppBar>
+      ) : (
+        <MobileNavBar />
+      )}
+    </>
+  );
 };
 
 export default NavBar;
